1f1bd3831b6d0a241708179cbdba9c0fa56f8b9e
Benn <benjaminchongzhiyi96@hotmail.com>
2018-05-18 11:06:14 +0800

    Model for list Items and associated migrations

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Jack heard about this new cool online to-do app
         # he goes check the homepage
@@ -41,16 +46,23 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Contact Charitable Organisations', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Contact Charitable Organisations')
 
         #There is still a text box inviting her to add another item.
         #He enters 'build app'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('build app')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        #The page updates again, now showing both his items in the list
+        self.check_for_row_in_list_table('1: Contact Charitable Organisations')
+        self.check_for_row_in_list_table('2: build app')
+
+        #He wonders whether the site will remember his list. He sees that
+        #the site has generated a unique URL for her
         self.fail('Finish the test!')
-
-    #The page updates again, now showing both his items in the list
-
-    #He wonders whether the site will remember his list. He sees that
-    #the site has generated a unique URL for her
 
     #she visits that url, and finds her to-do list is still there
 
